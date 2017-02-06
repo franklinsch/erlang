5b6f3f69741f7812272201a7271c00f4f314a5a4
Franklin Schrans <franklin.schrans14@imperial.ac.uk>
2017-02-06 13:56:42 +0000

    Change map format

--- cw1/client.erl
+++ cw1/client.erl
@@ -6,15 +6,18 @@ init(Name) ->
 
 neighbors(Name) ->
   receive {neighbors, Neighbors} -> 
-            Messages = maps:from_list([{P, 0} || P <- Processes]),
+            % Map format: {ProcessID, {NumReceived, NumSent}}
+            Messages = maps:from_list([{P, {0, 0}} || P <- Processes]),
             next(Name, Neighbors, Messages).
 
-next(Name, Neighbors, Received) ->
+next(Name, Neighbors, Messages) ->
   receive {message, Client} -> 
-          Received2 = maps:update(Client, Received#{Client} + 1, Received),
-          next(Name, Neighbors, Received2).
+          {Received, _} = Messages#{Client}
+          Messages2 = maps:update(Client, Received + 1, Received),
+          next(Name, Neighbors, Messages2).
   after 0 ->
           broadcast({message, self()}, Neighbors),
+          {Sent, _} = Messages
 
 broadcast(Message, Clients) ->
   [Client ! Message, Client <- Clients].
