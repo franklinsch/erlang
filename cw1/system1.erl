5b98a955b7df6bfabf7cbec6fb398d0ddad6e2b6
Franklin Schrans <f.schrans@me.com>
2017-02-07 08:18:16 +0000

    Remove FullyConnected

--- cw1/system1.erl
+++ cw1/system1.erl
@@ -4,14 +4,11 @@
 start(Args) ->
   [FirstArg | _] = Args,
   {NumClients, _} = string:to_integer(atom_to_list(FirstArg)),
-  Clients = spawnProcesses(NumClients, true),
+  Clients = spawnProcesses(NumClients),
   [ID ! {task1, start, 1000, 3000} || {_, ID} <- Clients].
 
-spawnProcesses(NumClients, FullyConnected) ->
+spawnProcesses(NumClients) ->
   ClientNames = lists:seq(1, NumClients),
-  Clients = [{Name, spawn(client, init, [Name])} || Name <- ClientNames],
-  case FullyConnected of
-    false -> Clients;
-    true  -> 
-      [ID ! {neighbors, Clients} || {_, ID} <- Clients]
-  end.
+  Clients = [{Name, spawn(client, init, [Name])} 
+             || Name <- ClientNames],
+  [ID ! {neighbors, Clients} || {_, ID} <- Clients].
