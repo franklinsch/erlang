731c9a5310f0ae9cafc2565defa2618edc4846c1
Franklin Schrans <f.schrans@me.com>
2017-02-07 08:11:30 +0000

    Implement sending

--- cw1/client.erl
+++ cw1/client.erl
@@ -6,18 +6,32 @@ init(Name) ->
 
 neighbors(Name) ->
   receive {neighbors, Neighbors} -> 
-            % Map format: {ProcessID, {NumReceived, NumSent}}
-            Messages = maps:from_list([{P, {0, 0}} || P <- Processes]),
-            next(Name, Neighbors, Messages).
+            task(Name, Neighbors)
+  end.
 
-next(Name, Neighbors, Messages) ->
-  receive {message, Client} -> 
-          {Received, _} = Messages#{Client}
-          Messages2 = maps:update(Client, Received + 1, Received),
-          next(Name, Neighbors, Messages2).
-  after 0 ->
-          broadcast({message, self()}, Neighbors),
-          {Sent, _} = Messages
+task(Name, Neighbors) ->
+  receive {task1, start, Max_messages, Timeout} ->
+            % Map format: {Name, NumReceived}}
+            Received = maps:from_list([{ClientName, {0, 0}} || 
+                                       {ClientName, _} <- Neighbors]),
+            timer:send_after(Timeout, timeout),
+            start(Max_messages, Timeout, Name, Neighbors, 0, Received)
+  end.
 
-broadcast(Message, Clients) ->
-  [Client ! Message, Client <- Clients].
+start(Max_messages, Timeout, Name, Neighbors, Sent, Received) ->
+  broadcast_after(rand:uniform(500), Neighbors, {message, Name}),
+  receive timeout ->
+            Communications = communications(Sent, Received),
+            io:format("~p: ~p", [Name, Communications]);
+          {message, ClientName} -> 
+            NumReceived = maps:get(ClientName, Received),
+            Received2 = maps:update(ClientName, NumReceived + 1, Received),
+            start(Max_messages, Timeout, Name, Neighbors, Sent, Received2)
+  end.  
+
+broadcast_after(Time, Clients, Message) ->
+  [timer:send_after(Time, Client, Message) || Client <- Clients].
+
+communications(Sent, Received) ->
+  Names = lists:sort(maps:keys(Received)),
+  string:join([{Name, {Sent, maps:get(Name, Received)}} || Name <- Names], " "). 
--- cw1/system1.erl
+++ cw1/system1.erl
@@ -5,7 +5,7 @@ start(Args) ->
   [FirstArg | _] = Args,
   {NumClients, _} = string:to_integer(atom_to_list(FirstArg)),
   Clients = spawnProcesses(NumClients, true),
-  [ID ! {task1, start, 1000, 3000}].
+  [ID ! {task1, start, 1000, 3000} || {_, ID} <- Clients].
 
 spawnProcesses(NumClients, FullyConnected) ->
   ClientNames = lists:seq(1, NumClients),
@@ -13,10 +13,5 @@ spawnProcesses(NumClients, FullyConnected) ->
   case FullyConnected of
     false -> Clients;
     true  -> 
-      [ID ! {neighbors, neighborsFor({Name, ID}, Clients)} || 
-                                                       {Name, ID} <- Clients]
+      [ID ! {neighbors, Clients} || {Name, ID} <- Clients]
   end.
-
-% Generates a list of neighbors for a node in a fully connected network.
-neighborsFor(Client, Clients) ->
-  lists:delete(Clients, Client).
