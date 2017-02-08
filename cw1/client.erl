b798357b734146cfeba997f4b0717a5eb69d9fff
Franklin Schrans <f.schrans@me.com>
2017-02-08 20:42:28 +0000

    Change NoSendLimit

--- cw1/client.erl
+++ cw1/client.erl
@@ -15,28 +15,29 @@ task(Name, Neighbors) ->
             Received = maps:from_list([{ClientName, 0} ||
                                        {ClientName, _} <- Neighbors]),
             timer:send_after(Timeout, timeout),
-            NoLimit = case Timeout of
+            NoSendLimit = case Timeout of
                         0 -> true;
                         _ -> false
                       end,
-            start(Max_messages, NoLimit, Timeout, Name, Neighbors, 0, Received)
+            start(Max_messages, NoSendLimit, Timeout, Name, Neighbors, 0, 
+                  Received)
   end.
 
-start(Max_messages, NoLimit, Timeout, Name, Neighbors, Sent, Received) ->
+start(Max_messages, NoSendLimit, Timeout, Name, Neighbors, Sent, Received) ->
   receive
     timeout ->
       printStats(Name, Sent, Received);
     {message, ClientName} ->
       NumReceived = maps:get(ClientName, Received),
       Received2 = maps:update(ClientName, NumReceived + 1, Received),
-      start(Max_messages, NoLimit, Timeout, Name, Neighbors, Sent, Received2)
+      start(Max_messages, NoSendLimit, Timeout, Name, Neighbors, Sent, Received2)
   after 0 ->
           if 
-            Max_messages == Sent and not NoLimit ->
+            Max_messages == Sent and not NoSendLimit ->
               printStats(Name, Sent, Received);
             true ->
               broadcast(Neighbors, {message, Name}),
-              start(Max_messages, NoLimit, Timeout, Name, Neighbors, Sent + 1,
+              start(Max_messages, NoSendLimit, Timeout, Name, Neighbors, Sent + 1,
                     Received)
           end
   end.
