375729c7b594f3f9fb3e3a13257e8e410c403901
Franklin Schrans <f.schrans@me.com>
2017-02-09 22:05:26 +0000

    Reformat system1

--- cw1/system1/system1.erl
+++ cw1/system1/system1.erl
@@ -9,6 +9,7 @@ start() ->
   [Process ! {task1, start, 1000, 3000} || {_, Process} <- Processes].
 
 spawnProcesses(NumProcesses) ->
-  Processes = [{ProcessID, spawn(client, init, [ProcessID])} || ProcessID <- lists:seq(1, NumProcesses)],
+  Processes = [{ProcessID, spawn(client, init, [ProcessID])} 
+               || ProcessID <- lists:seq(1, NumProcesses)],
   [Process ! {neighbors, Processes} || {_, Process} <- Processes],
   Processes.
