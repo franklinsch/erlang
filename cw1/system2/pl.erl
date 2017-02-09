4a852347b4ec80e634a628678a3022d6ead7a4b2
Franklin Schrans <f.schrans@me.com>
2017-02-09 20:33:07 +0000

    Bind client later

--- cw1/system2/pl.erl
+++ cw1/system2/pl.erl
@@ -1,21 +1,19 @@
 %%% Franklin Schrans (fs2014)
 
 -module(pl).
--export([init/1]).
+-export([init/0]).
 
-init(Client) ->
+init() ->
   PLs = bindPLs(),
-  next(Client, PLs).
+  receive {bind, Client} -> next(Client, PLs) 
+  end.
 
 bindPLs() ->
   receive {bind_pl, PLs} -> PLs
   end.
 
 next(Client, PLs) ->
-  receive {pl_send, M} ->
-            [PL ! M || PL <- PLs],
-            next(Client, PLs);
-          M ->
-            Client ! {pl_deliver, M},
-            next(Client, PLs)
-  end.
+  receive {pl_send, M} -> [PL ! M || PL <- PLs];
+          M -> Client ! {pl_deliver, M}
+  end,
+  next(Client, PLs).
