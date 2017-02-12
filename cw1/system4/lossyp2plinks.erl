6e09a56d182503a4172f366736544d206543d191
Franklin Schrans <f.schrans@me.com>
2017-02-12 09:59:20 +0000

    Implement unreliable sending

--- cw1/system4/lossyp2plinks.erl
+++ cw1/system4/lossyp2plinks.erl
@@ -1,21 +1,28 @@
 %%% Franklin Schrans (fs2014)
 
 -module(lossyp2plinks).
--export([init/0]).
+-export([init/1]).
 
-init() ->
+init(Reliability) ->
   PLs = bindPLs(),
   receive {bind_beb, BEB} -> 
-            receive {bind, Client} -> next(Client, PLs, BEB) 
+            receive {bind, Client} -> next(Client, PLs, BEB, Reliability) 
             end
   end.
 
 bindPLs() ->
   receive {bind_pls, PLs} -> maps:from_list(PLs) end.
 
-next(Process, PLs, BEB) ->
-  receive {pl_send, Dest, M} -> maps:get(Dest, PLs) ! {pl_msg, Process, M};
+next(Process, PLs, BEB, Reliability) ->
+  receive {pl_send, Dest, M} -> 
+            unreliableSend({pl_msg, Process, M}, maps:get(Dest, PLs), Reliability);
           {pl_msg, From, M} -> 
             BEB ! {pl_deliver, From, M}
   end,
-  next(Process, PLs, BEB).
+  next(Process, PLs, BEB, Reliability).
+
+unreliableSend(M, Dest, Reliability) ->
+  case rand:uniform(1, 100) < Reliability of
+    true -> Dest ! M;
+    false -> ok
+  end.
